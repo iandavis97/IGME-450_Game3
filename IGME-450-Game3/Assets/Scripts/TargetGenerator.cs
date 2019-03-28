2230363c7e84c167f80a9cc965cebcaa9ff9c3f5
Ryan Beach <rkb2204@rit.edu>
2019-03-28 17:16:01 -0400

    Made target practice scene
    made a scene for named target practice and a manager that handles collisions between the player's arm sections and the target, and respawns the target somewhere else.

--- IGME-450-Game3/Assets/Scripts/TargetGenerator.cs
+++ IGME-450-Game3/Assets/Scripts/TargetGenerator.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetGenerator : MonoBehaviour {
+public class TargetGenerator : MonoBehaviour
+{
+    // set up fields
+    public GameObject targetPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject upperArm;
+    public GameObject lowerArm;
+
+    private GameObject target;
+
+    private CircleCollider2D targetCollider;
+    private BoxCollider2D upperArmCollider;
+    private BoxCollider2D lowerArmCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateTarget();
+
+        targetCollider = target.GetComponent<CircleCollider2D>();
+        upperArmCollider = upperArm.GetComponent<BoxCollider2D>();
+        lowerArmCollider = lowerArm.GetComponent<BoxCollider2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (targetCollider.IsTouching(upperArmCollider))
+        {
+            Destroy(target);
+
+            CreateTarget();
+            targetCollider = target.GetComponent<CircleCollider2D>();
+        }
+        if (targetCollider.IsTouching(lowerArmCollider))
+        {
+            Destroy(target);
+
+            CreateTarget();
+            targetCollider = target.GetComponent<CircleCollider2D>();
+        }
+
+    }
+
+    // Creates a target randomly within the space of the stage
+    void CreateTarget()
+    {
+        target = Instantiate(targetPrefab, new Vector2(Random.Range(-4f, 4f), Random.Range(-.5f, 2f)), Quaternion.identity);
+    }
 }
