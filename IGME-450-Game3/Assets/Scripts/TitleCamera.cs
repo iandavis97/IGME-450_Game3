321037c5f9f9677c741b2d8040a00b57e057a13e
Jonathan So <jds7523@rit.edu>
2019-03-28 17:24:17 -0400

    Edited main menu and set it up to go to my scene.

---
+++ IGME-450-Game3/Assets/Scripts/TitleCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCamera : MonoBehaviour {
+
+	private Camera self;
+	private const float MIN_SIZE = 3f;
+	private float stepSize = 0.001f;
+
+	private void Awake() {
+		self = GetComponent<Camera>();
+	}
+
+	private void Update () {
+		if (self.orthographicSize > MIN_SIZE) {
+			transform.Translate(Vector2.right * -stepSize);
+			self.orthographicSize -= stepSize;
+		}
+	}
+}
