128e04df8d8718aaa2d9d4ec5efa4c3fcf82beda
Ryan Beach <rkb2204@rit.edu>
2019-03-28 15:40:09 -0400

    Set up a main menu scene
    I set up a main menu scene and gave it the methods needed to work, but it doesn't properly swich scenes yet, because nothing's loaded in the build settings.
    after setting up the build settings, it'll switch to malcolm's scene as it stands now.

---
+++ IGME-450-Game3/Assets/Scripts/SceneButton.cs
@@ -0,0 +1,20 @@
+﻿//handles changing scenes via button press
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour {
+
+	// changes the scene
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // ends the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
