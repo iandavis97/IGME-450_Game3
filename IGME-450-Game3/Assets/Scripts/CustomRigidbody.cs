f53e9ef20c6cd52764cf77f677bedc901c8c327e
Malcolm Lambrecht <jml1769@rit.edu>
2019-03-28 23:50:09 -0400

    WIP rigidbody tweaks, this commit is so i can pull other changes

---
+++ IGME-450-Game3/Assets/Scripts/CustomRigidbody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * attach this script to any object with a rigidbody that you want a non standard center of mass on
+ * currently doesnt work as intended
+ */
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CustomRigidbody : MonoBehaviour {
+
+    public Vector3 centerOfMass;
+    public float drawSize = 0.01f;
+    private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        centerOfMass = myRigidbody.centerOfMass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myRigidbody.centerOfMass = centerOfMass;
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(centerOfMass, drawSize);
+    }
+}
