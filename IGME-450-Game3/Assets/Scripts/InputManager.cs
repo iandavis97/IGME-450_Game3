60138067c927adc2d1509446f77e75080d99cd36
Jonathan So <jds7523@rit.edu>
2019-04-02 14:37:00 -0400

    Improved input manager to manually create the "controls" string
    + Set back up lost prefab / script references
    + Uses Malcolm's prefab
    + Hidden loss: click on "Punchout" on title

--- IGME-450-Game3/Assets/Scripts/InputManager.cs
+++ IGME-450-Game3/Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // For displaying the text string for controls.
 
 public class InputManager : MonoBehaviour {
 
@@ -8,8 +9,25 @@ public class InputManager : MonoBehaviour {
 	public KeyCode p1upArmAdd, p1upArmSub, p1loArmAdd, p1loArmSub, p1Left, p1Right, p2upArmAdd, p2upArmSub, p2loArmAdd, p2loArmSub, p2Left, p2Right;
 	public PlayerControl p1, p2; // PlayerControl objects for respective players.
 
+	private Text controls; // Text on the UI that prints out the controls.
+
 	private float torque = 5.0f; // Amount of torque to add to joints.
     private float distance = 0.05f; // Distance to move on walk
+
+    // Grab a reference to the text piece for controls.
+    // If Xbox One controller(s) are connected, detect them and override the keyboard controls.
+    private void Awake() {
+    	controls = Transform.FindObjectOfType<Text>();
+    	// #TODO Xbox Controller support
+    }
+
+    // Displays controls to the user.
+    private void Start() {
+		controls.text = "Player 1";
+		controls.text += ControlString(p1upArmAdd, p1upArmSub, p1loArmAdd, p1loArmSub, p1Left, p1Right);
+		controls.text += "\nPlayer 2";
+		controls.text += ControlString(p2upArmAdd, p2upArmSub, p2loArmAdd, p2loArmSub, p2Left, p2Right);
+    }
 
 	private void Update () {
 		// Player 1 Controls.
@@ -33,5 +51,19 @@ public class InputManager : MonoBehaviour {
         } else if (Input.GetKey(p2Right)) {
             p2.Walk(distance);
         }
+	}
+
+	/** Helper function that builds a string of the keycodes required to control the player.
+	 * param[upArmAdd, upArmSub, loArmAdd, loArmSub, left, right] - keycodes for a player's movement.
+	 * return - a string containing keycode information for controlling a player.
+	 */
+	private string ControlString(KeyCode upArmAdd, KeyCode upArmSub, KeyCode loArmAdd, KeyCode loArmSub, KeyCode left, KeyCode right) {
+		string result = "";
+		result += "\n" + upArmAdd.ToString() + " / " + upArmSub.ToString() + " - UPPER ARM";
+		result += "\n" + loArmAdd.ToString() + " / " + loArmSub.ToString() + " - LOWER ARM";
+		result += "\n";
+		result += "\n" + left.ToString() + " - MOVE LEFT";
+		result += "\n" + right.ToString() + " - MOVE RIGHT";
+		return result;
 	}
 }
