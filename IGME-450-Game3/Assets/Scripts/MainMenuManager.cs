7ba93126b0d43c76499cddb87f79ecc5cc9a86b2
Ryan Beach <rkb2204@rit.edu>
2019-04-04 20:55:32 -0400

    Create MainMenuManager.cs
    created a script to manage the controllable main menu

---
+++ IGME-450-Game3/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public GameObject quit;
+    public GameObject targetPractice;
+    public GameObject play;
+    public GameObject credits;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
